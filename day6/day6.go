005a3f53b39d8efb5e92a9273ebf97f37bd4cd67
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-12-06 09:21:28 +0100

    arr

--- day6/day6.go
+++ day6/day6.go
@@ -12,6 +12,13 @@ var laborMap = [][]string{}
 
 func Day6() {
 	readFile("day6.test")
+	fmt.Println(laborMap)
+
+	// find first position
+
+	// always find next position
+	// check if next position is already found
+	// if no -> add 1
 }
 
 func readFile(filepath string) {
@@ -23,5 +30,11 @@ func readFile(filepath string) {
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println(line)
+		str := []string{}
+		for i := 0; i < len(line); i++ {
+			char := line[i : i+1]
+			str = append(str, char)
+		}
+		laborMap = append(laborMap, str)
 	}
 }
