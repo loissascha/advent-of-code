fad7e41a00d198bde685eb2361320da7c326b22c
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-12-12 10:42:07 +0100

    reset

--- day12/day12.go
+++ day12/day12.go
@@ -8,92 +8,16 @@ import (
 	"github.com/loissascha/go-assert/assert"
 )
 
-type Plot struct {
-	char   string
-	fields map[int][]int
-}
-
 func Day12() {
 	m := readFile("day12.test")
 	for _, v := range m {
 		fmt.Println(v)
 	}
-	plots := findPlots(m)
-	fmt.Println(plots)
-	fmt.Println("amount plots:", len(plots))
-	for _, v := range plots {
-		amount := 0
-		for _, p := range v.fields {
-			amount += len(p)
-		}
-		fmt.Println(v.char, "with:", amount)
-	}
+	findPlots(m)
 }
 
-func findPlots(input [][]string) []Plot {
-	plots := []Plot{}
-	findPlot(input, 0, 0)
-	return plots
-}
+func findPlots(input [][]string) {
 
-func findPlot(input [][]string, x int, y int) {
-	foundPositions := make(map[int][]int)
-	foundPositions[0] = []int{0}
-	foundPositions = findDir(input, x+1, y, 1, 0, foundPositions)
-	foundPositions = findDir(input, x+1, y, -1, 0, foundPositions)
-	foundPositions = findDir(input, x+1, y, 0, -1, foundPositions)
-	foundPositions = findDir(input, x+1, y, 0, 1, foundPositions)
-	foundPositions = findDir(input, x-1, y, 1, 0, foundPositions)
-	foundPositions = findDir(input, x-1, y, -1, 0, foundPositions)
-	foundPositions = findDir(input, x-1, y, 0, -1, foundPositions)
-	foundPositions = findDir(input, x-1, y, 0, 1, foundPositions)
-	foundPositions = findDir(input, x, y+1, 1, 0, foundPositions)
-	foundPositions = findDir(input, x, y+1, -1, 0, foundPositions)
-	foundPositions = findDir(input, x, y+1, 0, -1, foundPositions)
-	foundPositions = findDir(input, x, y+1, 0, 1, foundPositions)
-	foundPositions = findDir(input, x, y-1, 1, 0, foundPositions)
-	foundPositions = findDir(input, x, y-1, -1, 0, foundPositions)
-	foundPositions = findDir(input, x, y-1, 0, -1, foundPositions)
-	foundPositions = findDir(input, x, y-1, 0, 1, foundPositions)
-	fmt.Println("find plot result for x", x, "y", y)
-	fmt.Println(foundPositions)
-}
-
-func hasFoundPosition(input map[int][]int, x int, y int) bool {
-	for yy, v := range input {
-		if y != yy {
-			continue
-		}
-		for _, n := range v {
-			fmt.Println("n is", n, "looking for", x)
-			if n == x {
-				fmt.Println("return true")
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func findDir(input [][]string, x int, y int, dirX int, dirY int, foundPositions map[int][]int) map[int][]int {
-	char := input[y][x]
-	fmt.Println("find dir for char", char, dirX, dirY)
-	if !hasFoundPosition(foundPositions, x, y) {
-		fmt.Println("has no found pos")
-		next := input[y+dirY][x+dirX]
-		fmt.Println("next is:", next)
-		if next == char && hasFoundPosition(foundPositions, x+dirX, y+dirY) {
-			fmt.Println("found pos: ", x, y)
-			ll := foundPositions[y]
-			ll = append(ll, x)
-			foundPositions[y] = ll
-			foundPositions = findDir(input, x+dirX, y+dirY, 1, 0, foundPositions)
-			foundPositions = findDir(input, x+dirX, y+dirY, -1, 0, foundPositions)
-			foundPositions = findDir(input, x+dirX, y+dirY, 0, 1, foundPositions)
-			foundPositions = findDir(input, x+dirX, y+dirY, 0, -1, foundPositions)
-		}
-	}
-	return foundPositions
 }
 
 func readFile(filepath string) [][]string {
