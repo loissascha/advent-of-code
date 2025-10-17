3f2ae72f7c902cc4cdf2d45327c220752579cbf2
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 13:09:08 +0200

    day 1 c

--- 2023/day1_c/main.c
+++ 2023/day1_c/main.c
@@ -3,5 +3,15 @@
 int main(int argc, char *argv[]) {
   puts("Hello world");
 
+  FILE *fptr;
+  fptr = fopen("test_input.txt", "r");
+
+  char myString[8];
+  while (fgets(myString, 8, fptr)) {
+    printf("read: %s\n", myString);
+  }
+
+  fclose(fptr);
+
   return 0;
 }
