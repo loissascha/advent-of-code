44a67b6fae6b4852d96fb86f3414e55904d72430
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 14:23:17 +0200

    zig

--- 2023/day1_zig/main.zig
+++ 2023/day1_zig/main.zig
@@ -1,9 +1,23 @@
 const std = @import("std");
 const print = std.debug.print;
 
-fn readFile(alloc: std.mem.Allocator, path: []const u8) ![]u8 {
+fn readFile(alloc: std.mem.Allocator, path: []const u8) ![]const u8 {
     const cwd = std.fs.cwd();
     return cwd.readFileAlloc(alloc, path, 4096);
+}
+
+fn getFileLines(alloc: std.mem.Allocator, path: []const u8) !std.ArrayList([]const u8) {
+    const fileContents = try readFile(alloc, path);
+    defer alloc.free(fileContents);
+    var lines = try std.ArrayList([]const u8).initCapacity(alloc, 16);
+
+    var splits = std.mem.splitAny(u8, fileContents, "\n");
+    while (splits.next()) |chunk| {
+        const lineCopy = try alloc.dupe(u8, chunk);
+        try lines.append(alloc, lineCopy);
+    }
+
+    return lines;
 }
 
 pub fn main() !void {
@@ -13,8 +27,13 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    const fileContents = try readFile(alloc, filename);
-    defer alloc.free(fileContents);
+    var lines = try getFileLines(alloc, filename);
+    defer {
+        for (lines.items) |line| alloc.free(line);
+        lines.deinit(alloc);
+    }
 
-    print("{s}", .{fileContents});
+    for (lines.items) |line| {
+        print("Line: {s}\n", .{line});
+    }
 }
