7994c5e582122310e70bcea6fe6f5f8bb520a66d
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 10:58:28 +0200

    get str num

--- 2023/day1_rust/src/main.rs
+++ 2023/day1_rust/src/main.rs
@@ -12,14 +12,87 @@ fn main() {
     println!("Sum: {}", sum);
 }
 
+fn get_str_number(input: &str) -> i32 {
+    // println!("get str number input {input}");
+    if input.contains("one") {
+        return 1;
+    }
+    if input.contains("two") {
+        return 2;
+    }
+    if input.contains("three") {
+        return 3;
+    }
+    if input.contains("four") {
+        return 4;
+    }
+    if input.contains("five") {
+        return 5;
+    }
+    if input.contains("six") {
+        return 6;
+    }
+    if input.contains("seven") {
+        return 7;
+    }
+    if input.contains("eight") {
+        return 8;
+    }
+    if input.contains("nine") {
+        return 9;
+    }
+    return 0;
+}
+
 fn get_numbers(line: &str) -> i32 {
+    println!("Get numbers for {line}");
     let chars = line.chars();
     let mut result = String::from("");
+
+    // find first number
+    let mut i = 0;
     for char in chars {
-        if char >= '0' && char <= '9' {
-            result.push(char);
+        // if the text up until this point might form a 'text' number (one, two, three, ...)
+        let sub: String = line.chars().take(i).collect();
+        let strnum = get_str_number(&sub);
+        if strnum > 0 {
+            result.push_str(&strnum.to_string());
+            break;
         }
+
+        // if it is a number
+        if char >= '0' && char <= '9' {
+            println!("char {char} is number");
+            result.push(char);
+            break;
+        }
+        i += 1;
     }
+
+    // find last number
+    let chars = line.chars();
+    let mut i = 0;
+    let length = line.chars().count();
+    for char in chars {
+        let idx = length - i - 1;
+        let sub: String = line.chars().skip(idx).collect();
+        println!("find last num sub: {sub}");
+        let strnum = get_str_number(&sub);
+        if strnum > 0 {
+            result.push_str(&strnum.to_string());
+            break;
+        }
+
+        // if it is a number
+        if char >= '0' && char <= '9' {
+            println!("char {char} is number");
+            result.push(char);
+            break;
+        }
+
+        i += 1;
+    }
+
     let length = result.chars().count();
     if length == 0 {
         return 0;
