f60ab520db675d2c189ff1d0e284efe8f88abb7e
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 10:26:09 +0200

    get lines

--- 2023/day1_rust/src/main.rs
+++ 2023/day1_rust/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "input_test.txt";
+    let parts = get_lines(file_path);
+    for part in parts {
+        println!("{}", part);
+    }
+}
+
+fn get_lines(file_path: &str) -> Vec<String> {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    contents.lines().map(|s| s.to_string()).collect()
 }
