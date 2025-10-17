1eb4685f6b459c7d187529665e4dc182af653727
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 14:27:47 +0200

    zig

--- 2023/day1_zig/main.zig
+++ 2023/day1_zig/main.zig
@@ -1,25 +1,6 @@
 const std = @import("std");
 const print = std.debug.print;
 
-fn readFile(alloc: std.mem.Allocator, path: []const u8) ![]const u8 {
-    const cwd = std.fs.cwd();
-    return cwd.readFileAlloc(alloc, path, 4096);
-}
-
-fn getFileLines(alloc: std.mem.Allocator, path: []const u8) !std.ArrayList([]const u8) {
-    const fileContents = try readFile(alloc, path);
-    defer alloc.free(fileContents);
-    var lines = try std.ArrayList([]const u8).initCapacity(alloc, 16);
-
-    var splits = std.mem.splitAny(u8, fileContents, "\n");
-    while (splits.next()) |chunk| {
-        const lineCopy = try alloc.dupe(u8, chunk);
-        try lines.append(alloc, lineCopy);
-    }
-
-    return lines;
-}
-
 pub fn main() !void {
     const filename = "test_input.txt";
 
@@ -36,4 +17,25 @@ pub fn main() !void {
     for (lines.items) |line| {
         print("Line: {s}\n", .{line});
     }
+}
+
+fn readFile(alloc: std.mem.Allocator, path: []const u8) ![]const u8 {
+    const cwd = std.fs.cwd();
+    return cwd.readFileAlloc(alloc, path, 4096);
+}
+
+fn getFileLines(alloc: std.mem.Allocator, path: []const u8) !std.ArrayList([]const u8) {
+    const fileContents = try readFile(alloc, path);
+    defer alloc.free(fileContents);
+    var lines = try std.ArrayList([]const u8).initCapacity(alloc, 16);
+
+    var splits = std.mem.splitAny(u8, fileContents, "\n");
+    while (splits.next()) |chunk| {
+        const lineCopy = try alloc.dupe(u8, chunk);
+        if (lineCopy.len > 0) {
+            try lines.append(alloc, lineCopy);
+        }
+    }
+
+    return lines;
 }
