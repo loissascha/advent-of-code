dab950365fbedb3c64cd16e30f8765b3fa621f2e
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-17 13:47:16 +0200

    zig

--- 2023/day1_zig/main.zig
+++ 2023/day1_zig/main.zig
@@ -1,22 +1,20 @@
 const std = @import("std");
 const print = std.debug.print;
 
+fn readFile(alloc: std.mem.Allocator, path: []const u8) ![]u8 {
+    const cwd = std.fs.cwd();
+    return cwd.readFileAlloc(alloc, path, 4096);
+}
+
 pub fn main() !void {
     const filename = "test_input.txt";
 
-    const fileContents = try read_file(filename);
-
-    print("{s}", .{fileContents});
-}
-
-fn read_file(filename: []const u8) ![]u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    const cwd = std.fs.cwd();
-    const fileContents = try cwd.readFileAlloc(alloc, filename, 4096);
-    // defer alloc.free(fileContents);
+    const fileContents = try readFile(alloc, filename);
+    defer alloc.free(fileContents);
 
-    return fileContents;
+    print("{s}", .{fileContents});
 }
