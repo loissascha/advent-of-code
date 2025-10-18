c98525a0841faa34b55c83dda8d3ef2c9542c28f
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-18 11:15:23 +0200

    setup

--- 2023/day4/main.go
+++ 2023/day4/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"day4/readfile"
+	"fmt"
+)
 
+func main() {
+	lines, err := readfile.ReadLines("test_input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	for line := range lines {
+		fmt.Println(line)
+	}
 }
