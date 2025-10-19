0e7110c0446efde11133977db14de612fc5eba74
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-19 20:25:24 +0200

    part 2

--- 2015/day1_rust/src/main.rs
+++ 2015/day1_rust/src/main.rs
@@ -4,12 +4,17 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read file");
     let chars: Vec<char> = input.chars().collect();
     let mut floor = 0;
+    let mut i = 0;
     for char in chars {
         if char == '(' {
             floor += 1;
         }
         if char == ')' {
             floor -= 1;
+        }
+        i += 1;
+        if floor == -1 {
+            println!("floor -1 at {}", i);
         }
     }
     println!("final floor: {}", floor);
