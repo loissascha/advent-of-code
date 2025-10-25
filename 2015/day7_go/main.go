9464ebe41d78dfef76585bc602c15cf78856bc0c
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-25 14:45:00 +0200

    variable done

--- 2015/day7_go/main.go
+++ 2015/day7_go/main.go
@@ -8,8 +8,10 @@ import (
 )
 
 type Variable struct {
-	Name  string
-	Value uint16
+	Name       string
+	Value      uint16
+	Operations []string
+	Done       bool
 }
 
 var variables map[string]Variable = map[string]Variable{}
@@ -25,8 +27,12 @@ func main() {
 		processLine(line)
 	}
 
+	fmt.Println(variables)
 	fmt.Println("var a value:", variables["a"].Value)
+	fmt.Println("var lx value:", variables["lx"].Value)
 }
+
+// TODO: a gate provides no signal until all of it's inputs have a signal
 
 func processLine(line string) {
 	if strings.TrimSpace(line) == "" {
@@ -36,71 +42,93 @@ func processLine(line string) {
 	action := split[0]
 	varname := split[1]
 
-	actionResult := getActionResult(action)
-
-	// if actionResult < 0 {
-	// 	actionResult = 65535 + actionResult + 1
-	// }
+	actionResult, found := getActionResult(action)
 
 	variables[varname] = Variable{
-		Name:  varname,
-		Value: actionResult,
+		Name:       varname,
+		Value:      actionResult,
+		Operations: []string{line},
+		Done:       found,
 	}
 
 	fmt.Println("Variable value: ", varname, actionResult)
 }
 
-func getActionResult(input string) uint16 {
+func getActionResult(input string) (uint16, bool) {
 	if strings.Contains(input, "AND") {
 		split := strings.Split(input, " AND ")
-		vara := variables[split[0]]
-		varb := variables[split[1]]
+		vara, found := variables[split[0]]
+		if !found {
+			return 0, false
+		}
+		varb, found := variables[split[1]]
+		if !found {
+			return 0, false
+		}
 		res := And(uint16(vara.Value), uint16(varb.Value))
 		fmt.Println("and res:", res)
-		return res
+		return res, true
 	} else if strings.Contains(input, "OR") {
 		split := strings.Split(input, " OR ")
-		vara := variables[split[0]]
-		varb := variables[split[1]]
+		vara, found := variables[split[0]]
+		if !found {
+			return 0, false
+		}
+		varb, found := variables[split[1]]
+		if !found {
+			return 0, false
+		}
 		res := Or(uint16(vara.Value), uint16(varb.Value))
 		fmt.Println("or res:", res)
-		return res
+		return res, true
 	} else if strings.Contains(input, "LSHIFT") {
 		split := strings.Split(input, " LSHIFT ")
-		vara := variables[split[0]]
+		vara, found := variables[split[0]]
+		if !found {
+			return 0, false
+		}
 		shift, err := strconv.Atoi(split[1])
 		if err != nil {
 			panic(err)
 		}
 		res := Lshift(uint16(vara.Value), uint16(shift))
 		fmt.Println("lshfit res:", res)
-		return res
+		return res, true
 	} else if strings.Contains(input, "RSHIFT") {
 		split := strings.Split(input, " RSHIFT ")
-		vara := variables[split[0]]
+		vara, found := variables[split[0]]
+		if !found {
+			return 0, false
+		}
 		shift, err := strconv.Atoi(split[1])
 		if err != nil {
 			panic(err)
 		}
 		res := Rshift(uint16(vara.Value), uint16(shift))
 		fmt.Println("rshfit res:", res)
-		return res
+		return res, true
 	} else if strings.Contains(input, "NOT") {
 		varStr := strings.TrimLeft(input, "NOT ")
-		vara := variables[varStr]
+		vara, found := variables[varStr]
+		if !found {
+			return 0, false
+		}
 		res := Not(uint16(vara.Value))
 		fmt.Println("not res:", res)
-		return res
+		return res, true
 	} else {
 		v, err := strconv.Atoi(input)
 		if err != nil {
 			// its a variable name
-			return variables[input].Value
+			v, found := variables[input]
+			if !found {
+				return 0, false
+			}
+			return v.Value, true
 		}
-		return uint16(v)
+		return uint16(v), true
 	}
-
-	return 0
+	panic("operation wrong")
 }
 
 func And(a, b uint16) uint16 {
