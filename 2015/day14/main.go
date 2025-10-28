4da20702b1f86484de00940124380d4d3b3901d1
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-28 17:54:06 +0100

    day 14

--- 2015/day14/main.go
+++ 2015/day14/main.go
@@ -20,11 +20,14 @@ type Reindeer struct {
 	FlyTime  int
 	RestTime int
 	State    ReindeerState
+	Points   int
 }
 
 func (r *Reindeer) simulateSeconds(second int) int {
+	fmt.Println("simulate seconds", second)
 	distance := 0
 	currentTime := 0
+	r.State = StateFly
 	for range second {
 		switch r.State {
 		case StateFly:
@@ -41,32 +44,33 @@ func (r *Reindeer) simulateSeconds(second int) int {
 				r.State = StateFly
 			}
 		}
+		fmt.Println("distance", distance)
 	}
 	return distance
 }
 
 func main() {
 
-	comet := &Reindeer{
-		Name:     "Comet",
-		FlySpeed: 14,
-		FlyTime:  10,
-		RestTime: 127,
-		State:    StateFly,
-	}
+	// comet := &Reindeer{
+	// 	Name:     "Comet",
+	// 	FlySpeed: 14,
+	// 	FlyTime:  10,
+	// 	RestTime: 127,
+	// 	State:    StateFly,
+	// }
+	//
+	// dancer := &Reindeer{
+	// 	Name:     "Dancer",
+	// 	FlySpeed: 16,
+	// 	FlyTime:  11,
+	// 	RestTime: 162,
+	// 	State:    StateFly,
+	// }
 
-	dancer := &Reindeer{
-		Name:     "Dancer",
-		FlySpeed: 16,
-		FlyTime:  11,
-		RestTime: 162,
-		State:    StateFly,
-	}
-
-	comatDistance := comet.simulateSeconds(1000)
-	dancerDistance := dancer.simulateSeconds(1000)
-	fmt.Println("comet distance:", comatDistance)
-	fmt.Println("dancer distance:", dancerDistance)
+	// comatDistance := comet.simulateSeconds(1000)
+	// dancerDistance := dancer.simulateSeconds(1000)
+	// fmt.Println("comet distance:", comatDistance)
+	// fmt.Println("dancer distance:", dancerDistance)
 
 	content, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -84,18 +88,44 @@ func main() {
 		reindeers = append(reindeers, r)
 	}
 
-	fmt.Println("there are", len(reindeers), "reindeers")
+	// fmt.Println("there are", len(reindeers), "reindeers")
+	//
+	// simulationTime := 2503
+	// longestDistance := 0
+	// for _, r := range reindeers {
+	// 	d := r.simulateSeconds(simulationTime)
+	// 	if d > longestDistance {
+	// 		longestDistance = d
+	// 	}
+	// }
+	//
+	// fmt.Println("longest distance:", longestDistance)
 
-	simulationTime := 2503
-	longestDistance := 0
-	for _, r := range reindeers {
-		d := r.simulateSeconds(simulationTime)
-		if d > longestDistance {
-			longestDistance = d
+	// tr := []*Reindeer{comet, dancer}
+	// simulateOverTime(tr, 1000)
+
+	simulateOverTime(reindeers, 2503)
+}
+
+func simulateOverTime(reindeers []*Reindeer, simulationTime int) {
+	for n := range simulationTime {
+		var winningReindeer *Reindeer
+		winningDistance := 0
+		for _, r := range reindeers {
+			d := r.simulateSeconds(n + 1)
+			if d > winningDistance {
+				winningDistance = d
+				winningReindeer = r
+			}
+		}
+		if winningReindeer != nil {
+			winningReindeer.Points++
 		}
 	}
 
-	fmt.Println("longest distance:", longestDistance)
+	for _, r := range reindeers {
+		fmt.Println(r.Name, "with points:", r.Points)
+	}
 }
 
 func lineToReindeer(line string) *Reindeer {
