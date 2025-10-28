2cecb4743ab17cc300ceb1bd77a00b940dc4cd1c
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-28 17:24:27 +0100

    test stuff

--- 2015/day14/main.go
+++ 2015/day14/main.go
@@ -1,5 +1,54 @@
 package main
 
+import "fmt"
+
+type ReindeerState int
+
+const (
+	StateFly  ReindeerState = 0
+	StateRest ReindeerState = 1
+)
+
+type Reindeer struct {
+	FlySpeed int
+	FlyTime  int
+	RestTime int
+	State    ReindeerState
+}
+
+func (r *Reindeer) simulateSeconds(second int) int {
+	distance := 0
+	currentTime := 0
+	for range second {
+		switch r.State {
+		case StateFly:
+			distance += r.FlySpeed
+			currentTime++
+			if currentTime >= r.FlyTime {
+				currentTime = 0
+				r.State = StateRest
+			}
+		case StateRest:
+			currentTime++
+			if currentTime >= r.RestTime {
+				currentTime = 0
+				r.State = StateFly
+			}
+		}
+	}
+	return distance
+}
+
 func main() {
+
+	comet := &Reindeer{
+		FlySpeed: 14,
+		FlyTime:  10,
+		RestTime: 127,
+		State:    StateFly,
+	}
+
+	comatDistance := comet.simulateSeconds(1000)
+	fmt.Println("comet distance:", comatDistance)
 
 }
