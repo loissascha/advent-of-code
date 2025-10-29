4b38b473af766a06f24ab5799770e5227046e1ea
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-29 08:10:00 +0100

    part 1 and 2

--- 2015/day15/main.go
+++ 2015/day15/main.go
@@ -23,39 +23,89 @@ func (i *Ingredient) getScore(spoons int) (c, d, f, t, ca int) {
 
 func main() {
 
-	butterscotch := &Ingredient{
-		Name:       "Butterscotch",
+	sprinkles := &Ingredient{
+		Name:       "Sprinkles",
+		Capacity:   5,
+		Durability: -1,
+		Flavor:     0,
+		Texture:    0,
+		Calories:   5,
+	}
+
+	peanutButter := &Ingredient{
+		Name:       "Peanut Butter",
 		Capacity:   -1,
-		Durability: -2,
-		Flavor:     6,
-		Texture:    3,
+		Durability: 3,
+		Flavor:     0,
+		Texture:    0,
+		Calories:   1,
+	}
+
+	frosting := &Ingredient{
+		Name:       "Frosting",
+		Capacity:   0,
+		Durability: -1,
+		Flavor:     4,
+		Texture:    0,
+		Calories:   6,
+	}
+
+	sugar := &Ingredient{
+		Name:       "Sugar",
+		Capacity:   -1,
+		Durability: 0,
+		Flavor:     0,
+		Texture:    2,
 		Calories:   8,
 	}
 
-	cinnamon := &Ingredient{
-		Name:       "Cinnamon",
-		Capacity:   2,
-		Durability: 3,
-		Flavor:     -2,
-		Texture:    -1,
-		Calories:   3,
-	}
+	// butterscotch := &Ingredient{
+	// 	Name:       "Butterscotch",
+	// 	Capacity:   -1,
+	// 	Durability: -2,
+	// 	Flavor:     6,
+	// 	Texture:    3,
+	// 	Calories:   8,
+	// }
+	//
+	// cinnamon := &Ingredient{
+	// 	Name:       "Cinnamon",
+	// 	Capacity:   2,
+	// 	Durability: 3,
+	// 	Flavor:     -2,
+	// 	Texture:    -1,
+	// 	Calories:   3,
+	// }
 
 	list := []*Ingredient{
-		butterscotch, cinnamon,
+		sprinkles, peanutButter, frosting, sugar,
 	}
 
 	highestScore := 0
 	for a := range 100 {
 		for b := range 100 {
-			if a+b == 100 {
-				butterscotch.Spoons = a
-				cinnamon.Spoons = b
-				score := getScore(list)
-				if score > highestScore {
-					highestScore = score
+			for c := range 100 {
+				for d := range 100 {
+					if a+b+c+d == 100 {
+						sprinkles.Spoons = a
+						peanutButter.Spoons = b
+						frosting.Spoons = c
+						sugar.Spoons = d
+						score := getScore(list)
+						if score > highestScore {
+							highestScore = score
+						}
+					}
 				}
 			}
+			// if a+b == 100 {
+			// 	butterscotch.Spoons = a
+			// 	cinnamon.Spoons = b
+			// 	score := getScore(list)
+			// 	if score > highestScore {
+			// 		highestScore = score
+			// 	}
+			// }
 		}
 	}
 
@@ -87,6 +137,8 @@ func getScore(ings []*Ingredient) int {
 	overallFla = max(overallFla, 0)
 	overallText = max(overallText, 0)
 	overallCal = max(overallCal, 0)
-	// overallCal = 1 // TODO: remove
-	return overallCap * overallDura * overallFla * overallText * overallCal
+	if overallCal != 500 {
+		return 0
+	}
+	return overallCap * overallDura * overallFla * overallText
 }
