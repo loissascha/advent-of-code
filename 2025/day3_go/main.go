2b9d739268119af229c40b91d3e894d9c98e8e61
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-12-03 15:38:34 +0100

    part 2

--- 2025/day3_go/main.go
+++ 2025/day3_go/main.go
@@ -20,7 +20,7 @@ func main() {
 		if line == "" {
 			continue
 		}
-		r, err := checkLine(line)
+		r, err := checkLinePart2(line)
 		if err != nil {
 			panic(err)
 		}
@@ -31,14 +31,61 @@ func main() {
 	fmt.Println("result:", result)
 }
 
+const NUM_LEN = 12
+
+func findLargestNumForIndex(nums []int, index int, startIndex int) (int, int) {
+	largestNumber := 0
+	largestIndex := 0
+	for n := startIndex + 1; n < len(nums); n++ {
+		minK := NUM_LEN - (index + 1)
+		k := len(nums) - (n + 1)
+		if k < minK {
+			continue
+		}
+
+		if nums[n] > largestNumber {
+			largestNumber = nums[n]
+			largestIndex = n
+		}
+	}
+	return largestNumber, largestIndex
+}
+
+func checkLinePart2(line string) (int, error) {
+	fmt.Println("line:", line)
+
+	nums := []int{}
+	for n := range len(line) {
+		this := string(line[n])
+		num, _ := strconv.Atoi(this)
+		nums = append(nums, num)
+	}
+
+	stack := make([]int, NUM_LEN)
+	lastIndex := -1
+
+	for i := range NUM_LEN {
+		stack[i], lastIndex = findLargestNumForIndex(nums, i, lastIndex)
+	}
+
+	fmt.Println("stack:", stack)
+
+	lc := ""
+	for n := range len(stack) {
+		lc = fmt.Sprintf("%s%d", lc, stack[n])
+	}
+	fmt.Println("lc:", lc)
+
+	lcn, _ := strconv.Atoi(lc)
+
+	return lcn, nil
+}
+
 func checkLine(line string) (int, error) {
 	fmt.Println("line:", line)
 	largestCombination := 0
 
 	for n := 0; n < len(line); n++ {
-		if n+1 == len(line) {
-			break
-		}
 		this := line[n]
 
 		for i := n + 1; i < len(line); i++ {
