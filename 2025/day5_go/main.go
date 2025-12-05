33c000d8af6ccfdb5e2fc67c79ad3bd2451e2d21
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-12-05 09:01:10 +0100

    part 2

--- 2025/day5_go/main.go
+++ 2025/day5_go/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"iter"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -16,6 +18,73 @@ func main() {
 	input := readFile("input.txt")
 	lines := strings.SplitSeq(input, "\n")
 
+	part2(lines)
+}
+
+func part2(lines iter.Seq[string]) {
+	freshIdRange := []Range{}
+	for line := range lines {
+		if strings.Contains(line, "-") {
+			fmt.Println("line:", line)
+			sp := strings.SplitN(line, "-", 2)
+
+			freshIdRange = append(freshIdRange, Range{
+				Start: strToInt(sp[0]),
+				End:   strToInt(sp[1]),
+			})
+
+			continue
+		}
+	}
+
+	slices.SortFunc(freshIdRange, func(a, b Range) int {
+		if a.Start < b.Start {
+			return -1
+		}
+		if a.Start > b.Start {
+			return 1
+		}
+		return 0
+	})
+
+	found := true
+	for found {
+		found = false
+		for n, item := range freshIdRange {
+			if n+1 == len(freshIdRange) {
+				break
+			}
+			next := freshIdRange[n+1]
+
+			if isInRange(item.Start, item.End, next.Start) || isInRange(item.Start, item.End, next.End) {
+				newStart := min(item.Start, next.Start)
+				newEnd := max(next.End, item.End)
+				freshIdRange[n].Start = newStart
+				freshIdRange[n].End = newEnd
+				freshIdRange = slices.Delete(freshIdRange, n+1, n+2)
+				found = true
+				break
+			}
+		}
+	}
+
+	sum := 0
+	for _, item := range freshIdRange {
+		sum += item.End - (item.Start - 1)
+	}
+
+	fmt.Println("sum:", sum)
+
+}
+
+func isInRange(start, end, pos int) bool {
+	if start <= pos && end >= pos {
+		return true
+	}
+	return false
+}
+
+func part1(lines iter.Seq[string]) {
 	// freshIdMap := map[int]bool{}
 	// freshIdSlice := []int{}
 	freshIdRange := []Range{}
@@ -68,6 +137,7 @@ func main() {
 	}
 
 	fmt.Println("fresh:", freshs)
+
 }
 
 func strToInt(s string) int {
