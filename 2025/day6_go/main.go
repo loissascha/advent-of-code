8104608a6a7fef6125355e216ca7ac4fb0656b3f
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-12-07 09:52:47 +0100

    part 2

--- 2025/day6_go/main.go
+++ 2025/day6_go/main.go
@@ -10,78 +10,99 @@ import (
 func main() {
 	input := readFile("input.txt")
 
-	var actions []string
-	nums := [][]int{}
-
 	lines := strings.Split(input, "\n")
-	for n, line := range lines {
+	fmt.Println("first line:", lines[0])
+
+	lineSlices := [][][]string{}
+	maxLen := 0
+	for _, line := range lines {
 		if line == "" {
 			continue
 		}
-		fmt.Println(line)
-		fmt.Println(n, len(lines))
-		if n < len(lines)-2 {
-			slice := getLineSlice(line)
-			fmt.Println(slice)
-			nums = append(nums, slice)
-		} else {
-			// last line
-			actions = getActionsLine(line)
-			fmt.Println(actions)
-		}
+		lineSlice := parseLine(line)
+		maxLen = len(lineSlice)
+		lineSlices = append(lineSlices, lineSlice)
 	}
+
+	fmt.Println("max len:", maxLen)
+	fmt.Println("line slices:", lineSlices)
 
 	result := 0
-	for n, a := range actions {
-		fmt.Println("action:", a)
-		all := []int{}
-		for _, num := range nums {
-			all = append(all, num[n])
-		}
-		fmt.Println("with:", all)
+	for pos := range maxLen {
+		fmt.Println("pos:", pos)
 
-		startNum := all[0]
-		for i := 1; i < len(all); i++ {
-			switch a {
-			case "*":
-				startNum *= all[i]
-			case "+":
-				startNum += all[i]
+		firsts := ""
+		seconds := ""
+		thirds := ""
+		symbol := ""
+
+		for _, lineSlice := range lineSlices {
+			ls := lineSlice[pos]
+			fmt.Println(ls)
+
+			first := ls[0]
+			second := ls[1]
+			third := ls[2]
+
+			if first == "*" {
+				symbol = "*"
+				continue
 			}
+			if first == "+" {
+				symbol = "+"
+				continue
+			}
+
+			firsts = fmt.Sprintf("%s%s", firsts, first)
+			seconds = fmt.Sprintf("%s%s", seconds, second)
+			thirds = fmt.Sprintf("%s%s", thirds, third)
 		}
-		result += startNum
+
+		fmt.Println(firsts, seconds, thirds, symbol)
+		firstN, _ := strconv.Atoi(strings.TrimSpace(firsts))
+		secondN, _ := strconv.Atoi(strings.TrimSpace(seconds))
+		thirdN, _ := strconv.Atoi(strings.TrimSpace(thirds))
+		fmt.Println(firstN, secondN, thirdN, symbol)
+
+		switch symbol {
+		case "*":
+			result += (firstN * secondN * thirdN)
+		case "+":
+			result += (firstN + secondN + thirdN)
+		default:
+			panic("Symbold not found!: " + symbol)
+		}
 	}
+
 	fmt.Println("result:", result)
 }
 
-func getActionsLine(line string) []string {
-	split := strings.SplitSeq(line, " ")
-	items := []string{}
-	for l := range split {
-		l = strings.TrimSpace(l)
-		if l == "" {
-			continue
-		}
-		items = append(items, l)
-	}
-	return items
-}
+func parseLine(line string) [][]string {
+	fmt.Println("parse line:", line)
 
-func getLineSlice(line string) []int {
-	split := strings.SplitSeq(line, " ")
-	items := []int{}
-	for l := range split {
-		l = strings.TrimSpace(l)
-		if l == "" {
-			continue
+	lineSlice := [][]string{}
+
+	parsing := 0
+	chars := make([]string, 0, 3)
+	for _, char := range line {
+		if parsing < 3 {
+			chars = append(chars, string(char))
 		}
-		n, err := strconv.Atoi(l)
-		if err != nil {
-			panic(err)
+
+		parsing++
+
+		switch parsing {
+		case 3: // parsed 3 chars -> put them in the slice
+			lineSlice = append(lineSlice, chars)
+			chars = make([]string, 0, 3)
+		case 4:
+			// one free space before the next parsing begins
+			parsing = 0
 		}
-		items = append(items, n)
 	}
-	return items
+
+	fmt.Println("parsed line:", lineSlice)
+	return lineSlice
 }
 
 func readFile(path string) string {
